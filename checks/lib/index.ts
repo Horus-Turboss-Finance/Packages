253e0a07d613df7ca8dd594d448751f47129a97d
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-08-08 12:38:00 +0200

    feat(checks): ajout de la vérification port pour gestion dynamique

--- checks/lib/index.ts
+++ checks/lib/index.ts
@@ -1,4 +1,5 @@
 import { validate } from "email-validator";
+import { check } from "tcp-port-used";
 
 /**
  * Vérifie si c'est un nombre
@@ -35,4 +36,34 @@ export function stringCheck (entityToCheck : any) : boolean{
  */
 export function emailCheck (entityToCheck : any) : boolean{
     return stringCheck(entityToCheck) && validate(entityToCheck.toLowerCase())
+}
+
+/**
+ * Vérifie le premier port libre (après 1k)
+ * @param {string} IP
+ * @returns {number}
+ * 
+ * @example
+ * import { FreePort } from "checks";
+ *
+ *  let main = async () => {
+ *    console.log(await FreePort())
+ *  }
+ *  
+ * main()
+ */
+export async function FreePort (IP = "127.0.0.1") : Promise<number> {
+    return new Promise(async (res, rej) => {
+        try{
+            for(let i = 1000; i < 60000; i++){
+                let isFree = await check(i, IP)
+                if(isFree){
+                    res(i)
+                    break
+                }
+            }
+        }catch(e){
+            rej(e)
+        }
+    })
 }
