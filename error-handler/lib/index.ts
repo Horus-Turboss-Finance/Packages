3a4a54d6e5c690d752a6d50750a8edb2a25c23fe
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-10-13 10:42:55 +0200

    fix(error-handler): correction fonction appels des erreurs

--- error-handler/lib/index.ts
+++ error-handler/lib/index.ts
@@ -14,61 +14,84 @@ export class ResponseException extends Error {
   UnknownError = () => {
     this.title = "INTERNAL SERVER ERROR"
     this.status = 500
+
+    return this.send()
   }
 
   MethodNotAllowed = () => {
     this.title = "METHOD NOT ALLOWED"
     this.status = 405
+
+    return this.send()
   }
 
   PaymentRequired = () => {
     this.title = "PAYMENT REQUIRED"
     this.status = 402
+
+    return this.send()
   }
 
   ToManyRequest = () => {
     this.title = "TO MANY REQUEST"
     this.status = 429
+
+    return this.send()
   }
 
   Unauthorized = () => {
     this.title = "UNAUTHORIZED"
     this.status = 401
+
+    return this.send()
   }
 
   BadRequest = () => {
     this.title = "BAD REQUEST"
     this.status = 400
+
+    return this.send()
   }
 
   IMATeapot = () => {
     this.title = "I'M A TEAPOT"
     this.status = 41
+
+    return this.send()
   }
 
   Forbidden = () => {
     this.title = "FORBIDDEN"
     this.status = 403
+
+    return this.send()
   }
 
   NotFound = () => {
     this.title = "NOT FOUND"
     this.status = 404
+
+    return this.send()
   }
 
   OK = () => {
     this.title = "OK"
     this.status = 201
+
+    return this.send()
   }
 
   Success = () => {
     this.title = "SUCCESS"
     this.status = 200
+
+    return this.send()
   }
 
 
   send = () => {
     const success = this.status < 400;
+    
     return {
       success : success,
       title : this.title,
