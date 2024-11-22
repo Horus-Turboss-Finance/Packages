83228d88a506dd12db87e483ef224792a1c74150
Docteur_Turboss <81076765+docteur-turboss@users.noreply.github.com>
2024-11-22 21:52:58 +0100

    feat(adressManager): ajout des fonctions adressManager

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * as utils from "./utils/utils";
 export * as params from "./params/params";
 export * as middleware from "./middleware/middleware"
+export * as servicesConnexion from './adressManager/adressManager'
 
 export * from "./log/log";
 export * from "./date/date";
